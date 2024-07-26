56ed4bf6d0a1c6960185848362b742e3d2e43b12
Abhinav Karn <abhinav.karn@globant.com>
2024-07-26 11:52:39 +0530

    fixed all errors

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -8,9 +8,28 @@ export default async function decorate(block) {
   const [quotation, attribution] = [...block.children].map((c) => c.firstElementChild);
   const blockquote = document.createElement('blockquote');
 
-  // decoration attribution
-  if (attribution) {
-    console.log(attribution.innerHTML);
+  const API_KEY = "AIzaSyDiLLehJXY7hQ-25vJuibkZ9TzFsIjMNRg";
+  // Access your API key (see "Set up your API key" above)
+  const genAI = new GoogleGenerativeAI(API_KEY);
+  // The Gemini 1.5 models are versatile and work with most use cases
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  let finalQuote = '';
+  async function run() {
+    let promptVal = 'Give a Quote by ';
+    const prompt =  promptVal + attribution.innerText.split(',')[1].trim();
+    console.log('prompt - ' + prompt);
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const finalQuote = response.text().replaceAll('"','');
+    console.log('finalQuote - ' + finalQuote);
+
+    quotation.className = 'quote-quotation';
+    if (!hasWrapper(quotation)) {
+      quotation.innerHTML = `<p>${finalQuote}</p>`;
+    }
+    blockquote.append(quotation);
+
     attribution.className = 'quote-attribution';
     if (!hasWrapper(attribution)) {
       attribution.innerHTML = `<p>${attribution.innerHTML}</p>`;
@@ -24,29 +43,11 @@ export default async function decorate(block) {
     });
   }
 
-  const API_KEY = "AIzaSyDiLLehJXY7hQ-25vJuibkZ9TzFsIjMNRg";
-  // Access your API key (see "Set up your API key" above)
-  const genAI = new GoogleGenerativeAI(API_KEY);
-  // The Gemini 1.5 models are versatile and work with most use cases
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-  async function run() {
-    let promptVal = 'Give a Quote by -';
-    const prompt =  promptVal + attribution.innerText.split(',')[1].trim();
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const text = response.text();
-    console.log(text);
+  // decoration attribution
+  if (attribution) {
+    run();
   }
 
-  run();
-
-  // decorate quotation
-  quotation.className = 'quote-quotation';
-  if (!hasWrapper(quotation)) {
-    quotation.innerHTML = `<p>${quotation.innerHTML}</p>`;
-  }
-  blockquote.append(quotation);
   block.innerHTML = '';
   block.append(blockquote);
 }
