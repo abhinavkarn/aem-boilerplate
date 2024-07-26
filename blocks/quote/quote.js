038a90481cff7fb5e2086df7f73e2a0759f0c45f
Abhinav Karn <abhinav.karn@globant.com>
2024-07-26 10:40:51 +0530

    added gemini integration to quote

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -5,14 +5,10 @@ function hasWrapper(el) {
 export default async function decorate(block) {
   const [quotation, attribution] = [...block.children].map((c) => c.firstElementChild);
   const blockquote = document.createElement('blockquote');
-  // decorate quotation
-  quotation.className = 'quote-quotation';
-  if (!hasWrapper(quotation)) {
-    quotation.innerHTML = `<p>${quotation.innerHTML}</p>`;
-  }
-  blockquote.append(quotation);
+
   // decoration attribution
   if (attribution) {
+    console.log(attribution.innerHTML);
     attribution.className = 'quote-attribution';
     if (!hasWrapper(attribution)) {
       attribution.innerHTML = `<p>${attribution.innerHTML}</p>`;
@@ -25,6 +21,48 @@ export default async function decorate(block) {
       em.replaceWith(cite);
     });
   }
+
+  async function run() {
+    const prompt = "Give a Quote by - " + attribution.innerHTML;
+    const result = await geminiModel.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+  }
+
+  run();
+
+  // decorate quotation
+    quotation.className = 'quote-quotation';
+    if (!hasWrapper(quotation)) {
+      quotation.innerHTML = `<p>${quotation.innerHTML}</p>`;
+    }
+    blockquote.append(quotation);
+
   block.innerHTML = '';
   block.append(blockquote);
 }
+
+
+
+
+    // Fetch your API_KEY
+
+    // Reminder: This should only be for local testing
+
+    // Access your API key (see "Set up your API key" above)
+
+
+    // The Gemini 1.5 models are versatile and work with most use cases
+
+
+    async function run() {
+      const prompt = "Write a story about a magic backpack.";
+
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text();
+      console.log(text);
+    }
+
+    run();
