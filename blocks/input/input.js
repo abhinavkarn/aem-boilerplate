e0670ed67af24b746a574aac703916f5eea39f02
Abhinav Karn <abhinav.karn@globant.com>
2024-07-28 11:39:36 +0530

    Added CSS for Input

--- blocks/input/input.js
+++ blocks/input/input.js
@@ -2,7 +2,7 @@ export default async function decorate(block) {
   if (block.className.includes("input")){
     let inputBlock = document.createElement("input");
     inputBlock.setAttribute('type', 'text');
-    inputBlock.setAttribute('value', 'default');
+    inputBlock.setAttribute('value', 'Enter your Query');
     block.append(inputBlock);
   }
 }
