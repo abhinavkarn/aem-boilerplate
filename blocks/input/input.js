eb3309d738a161c981afda18d7001952ec56acba
Abhinav Karn <abhinav.karn@globant.com>
2024-07-29 11:01:16 +0530

    Update input.js

--- blocks/input/input.js
+++ blocks/input/input.js
@@ -1,24 +1,44 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
+const API_KEY = 'AIzaSyDiLLehJXY7hQ-25vJuibkZ9TzFsIjMNRg';
+const genAI = new GoogleGenerativeAI(API_KEY);
+const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+const chat = model.startChat({
+  history: [],
+  generationConfig: {
+    maxOutputTokens: 100,
+  },
+});
+
+
 function handleKeyPress(e) {
   if (e.code === 'Enter') {
     e.preventDefault();
-    const newMsg = document.getElementById('message').value;
-    const logBlock = document.getElementById('log');
-    const linebreak = document.createElement('br');
-    logBlock.appendChild(linebreak);
-    logBlock.append(`Query: ${newMsg}`);
-    window.runConversation(newMsg);
+    const query = document.getElementById('message').value;
+    runConversation(query);
     const getInputBox = document.getElementById('message');
     getInputBox.value = '';
   }
 }
 
-export default async function decorate(block) {
+async function runConversation(query) {
 
-  const API_KEY = 'AIzaSyDiLLehJXY7hQ-25vJuibkZ9TzFsIjMNRg';
-  const genAI = new GoogleGenerativeAI(API_KEY);
-  const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+  const logBlock = document.getElementById('log');
+  const linebreak = document.createElement('br');
+  logBlock.appendChild(linebreak);
+  logBlock.append(`Query: ${query}`);
+
+  const result = await chat.sendMessage(query);
+  const response = await result.response;
+  const text = response.text();
+  const secondBlock = document.getElementById('log');
+  const secondLineBreak = document.createElement('br');
+  secondBlock.append(secondLineBreak);
+  secondBlock.append(`Response: ${text}`);
+}
+
+
+export default async function decorate(block) {
 
   if (block.className.includes('input')){
     let log = document.createElement('p');
@@ -33,29 +53,6 @@ export default async function decorate(block) {
   }
 
   const msg = 'I would like to know about Tesla Model 3?';
-  const logBlock = document.getElementById('log');
-  const linebreak = document.createElement('br');
-  logBlock.appendChild(linebreak);
-  logBlock.append(`Query: ${msg}`);
-  const chat = model.startChat({
-    history: [],
-    generationConfig: {
-      maxOutputTokens: 100,
-    },
-  });
-
-  async function runConversation(newText) {
-    const result = await chat.sendMessage(newText);
-    const response = await result.response;
-    const text = response.text();
-    const logBlock = document.getElementById('log');
-    const linebreak = document.createElement('br');
-    logBlock.appendChild(linebreak);
-    logBlock.append(`Response: ${text}`);
-    //console.log(text);
-  }
-
-  window.runConversation = runConversation;
   runConversation(msg);
 
   const getInputBox = document.getElementById('message');
