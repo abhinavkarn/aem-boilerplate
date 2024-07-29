92391db4b1955eed0b2553a16b1980ba186f9174
Abhinav Karn <abhinav.karn@globant.com>
2024-07-29 11:42:18 +0530

    Update input.js

--- blocks/input/input.js
+++ blocks/input/input.js
@@ -10,6 +10,19 @@ const chat = model.startChat({
   },
 });
 
+async function runConversation(query) {
+  const logBlock = document.getElementById('log');
+  const linebreak = document.createElement('br');
+  logBlock.appendChild(linebreak);
+  logBlock.append(`Query: ${query}`);
+  const result = await chat.sendMessage(query);
+  const response = await result.response;
+  const text = response.text();
+  const secondBlock = document.getElementById('log');
+  const secondLineBreak = document.createElement('br');
+  secondBlock.append(secondLineBreak);
+  secondBlock.append(`Response: ${text}`);
+}
 
 function handleKeyPress(e) {
   if (e.code === 'Enter') {
@@ -21,40 +34,19 @@ function handleKeyPress(e) {
   }
 }
 
-async function runConversation(query) {
-
-  const logBlock = document.getElementById('log');
-  const linebreak = document.createElement('br');
-  logBlock.appendChild(linebreak);
-  logBlock.append(`Query: ${query}`);
-
-  const result = await chat.sendMessage(query);
-  const response = await result.response;
-  const text = response.text();
-  const secondBlock = document.getElementById('log');
-  const secondLineBreak = document.createElement('br');
-  secondBlock.append(secondLineBreak);
-  secondBlock.append(`Response: ${text}`);
-}
-
-
 export default async function decorate(block) {
-
-  if (block.className.includes('input')){
+  if (block.className.includes('input')) {
     let log = document.createElement('p');
     log.setAttribute('id', 'log');
     block.append(log);
-
     let inputBlock = document.createElement('input');
     inputBlock.setAttribute('type', 'text');
     inputBlock.setAttribute('id', 'message');
     inputBlock.setAttribute('value', 'Enter your Query');
     block.append(inputBlock);
   }
-
   const msg = 'I would like to know about Tesla Model 3?';
   runConversation(msg);
-
   const getInputBox = document.getElementById('message');
   getInputBox.addEventListener('keyup', handleKeyPress);
 }
